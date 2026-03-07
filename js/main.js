64e169de63aaebebc73c390b0cbc72f764fd6e40
Alberto Cabeza <venezuelawiro2@gmail.com>
2026-03-07 15:40:08 -0400

    effect glitch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//========================================================================================
+// ABRIR MENU DESPLEGABLE CON BOTON
 // Obtener elementos
 const dropdownButton = document.getElementById("dropdownButton");
 const dropdownMenu = document.getElementById("dropdownMenu");
@@ -9,7 +11,7 @@ function toggleMenu(event) {
   dropdownButton.classList.toggle("active"); // Añade/quita la clase active
 }
 
-// Abrir/cerrar al hacer clic en el botón
+// Abrir/cerrar al hacer clic en el botón.
 dropdownButton.addEventListener("click", toggleMenu);
 
 // Cerrar el menú si se hace clic fuera de él
@@ -29,5 +31,31 @@ document.addEventListener("keydown", function (event) {
   if (event.key === "Escape" && dropdownMenu.classList.contains("show")) {
     dropdownMenu.classList.remove("show");
     dropdownButton.classList.remove("active"); // Quita la clase active
+  }
+});
+//========================================================================================
+
+//========================================================================================
+// Esperar a que el DOM esté listo
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleccionamos del <a>
+  const logoLink = document.querySelector(".barra-navegacion > a");
+
+  if (logoLink) {
+    logoLink.addEventListener("click", function (e) {
+      //Si el enlace apunta a # o no quieres navegar deja la linea de abajo,
+      // sino eliminala
+      e.preventDefault();
+
+      // Añadir clase glitch
+      this.classList.add("glitch-activo");
+
+      // Quitarla despues de 300ms (Debe ser igual que la duración del keyframe)
+      setTimeout(() => {
+        this.classList.remove("glitch-activo");
+      }, 300);
+    });
+  } else {
+    console.log("No se encontró el enlace del logo. Verifica la estructura");
   }
 });
